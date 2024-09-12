91083d0d83bc371a809c5ba23d32b120143ed3f5
fahd alghtni <fahdalghtni246@gmail.com>
2024-09-12 11:31:38 +0300

    W3-D5-lab1

---
+++ min.js
@@ -0,0 +1,15 @@
+// تعريف المتغيرات
+let text = document.getElementsByTagName("h1");
+
+
+function changeText() {
+    text.textContent = "hello     lllll";
+}
+
+
+function increaseSize() {
+    
+    text.style.fontSize = currentFontSize + "px";
+}
+
+
